e50bf371abb10e578cd45c9b83e1cff1272afb7f
sahil-mehra <mehrasahil331997@gmail.com>
2024-03-29 15:06:54 +0530

    Added code

--- Weather-app/src/app/app.component.ts
+++ Weather-app/src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+interface WeatherData {
+  name: string;
+  main: { temp: number };
+  weather: { description: string }[];
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,FormsModule,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'Weather-app';
+  city: string = '';
+  weatherData: WeatherData | null = null;
+  error: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  getWeather() {
+    const apiKey = '14f0aa872f37c96c45c486595a9ed74d'; // Replace with your OpenWeatherMap API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apiKey}&units=metric`;
+
+    this.http.get<WeatherData>(apiUrl)
+      .subscribe(
+        (response) => {
+          this.weatherData = response;
+          this.error = null;
+        },
+        (error) => {
+          this.weatherData = null;
+          this.error = 'Error fetching weather data.';
+        }
+      );
+  }
 }
